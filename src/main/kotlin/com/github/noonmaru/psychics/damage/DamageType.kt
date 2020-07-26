1120f151f446f16d2460fbf5730c0d5f77dd0d7a
noonmaru <noonmaru10@gmail.com>
2020-07-27 07:14:05 +0900

    Add DamageType

---
+++ src/main/kotlin/com/github/noonmaru/psychics/damage/DamageType.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020 Noonmaru
+ *
+ *  Licensed under the General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/gpl-3.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.noonmaru.psychics.damage
+
+import org.bukkit.enchantments.Enchantment
+
+enum class DamageType(
+    val protection: Enchantment?,
+    val i18Name: String
+) {
+    MELEE(null, "근접"), // 보호
+    RANGED(Enchantment.PROTECTION_PROJECTILE, "원거리"), // 원거리 보호
+    FIRE(Enchantment.PROTECTION_FIRE, "화염"), // 화염 보호
+    BLAST(Enchantment.PROTECTION_EXPLOSIONS, "폭발"), // 폭발 보호
+}
