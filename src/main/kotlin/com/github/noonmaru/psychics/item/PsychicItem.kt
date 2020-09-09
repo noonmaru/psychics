c8bc7599c317a1c10c6179acdff80a58a604ece8
Noonmaru <noonmaru10@gmail.com>
2020-09-10 01:25:45 +0900

    Add ItemMeta

--- src/main/kotlin/com/github/noonmaru/psychics/item/PsychicItem.kt
+++ src/main/kotlin/com/github/noonmaru/psychics/item/PsychicItem.kt
@@ -3,6 +3,7 @@ package com.github.noonmaru.psychics.item
 import net.md_5.bungee.api.ChatColor
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
 
 object PsychicItem {
     val boundTag = "${ChatColor.RED}${ChatColor.BOLD} 능력 귀속"
@@ -24,6 +25,23 @@ var ItemStack.isPsychicbound
             if (lore.remove(PsychicItem.boundTag)) {
                 meta.lore = lore
                 itemMeta = meta
+            }
+        }
+    }
+
+var ItemMeta.isPsychicbound
+    get() = lore?.let { PsychicItem.boundTag in it } ?: false
+    set(value) {
+        val lore = lore ?: ArrayList<String>(1)
+
+        if (value) {
+            if (PsychicItem.boundTag !in lore) {
+                lore.add(PsychicItem.boundTag)
+                this.lore = lore
+            }
+        } else {
+            if (lore.remove(PsychicItem.boundTag)) {
+                this.lore = lore
             }
         }
     }
